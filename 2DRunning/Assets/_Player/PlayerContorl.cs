1d58f117c1e960104f1a9a5fda76b103de5d998b
RoanAlen <88921086+RoanAlen@users.noreply.github.com>
2022-01-08 18:51:56 +0800

    Audio

--- 2DRunning/Assets/_Player/PlayerContorl.cs
+++ 2DRunning/Assets/_Player/PlayerContorl.cs
@@ -8,21 +8,26 @@ public class PlayerContorl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        AnimatorStateInfo stateInfo;
+        stateInfo = playerAnim.GetCurrentAnimatorStateInfo(0);
+        if (Input.GetKeyDown(KeyCode.Space) )
         {
             Debug.Log("按下空白鍵");
             Anim("Player", "Player_Jump");
         }
-        if (Input.GetKey(KeyCode.LeftShift))
+        if (Input.GetKey(KeyCode.LeftShift) )
         {
+            if (!stateInfo.IsName("Player_Jump")) 
+            {
+                Anim("Player", "Player_Swipe");
+            }
             Debug.Log("滑行");
-            Anim("Player", "Player_Swipe");
         }
     }
     public void Anim(string AnimLayerName,string AnimStateName)
